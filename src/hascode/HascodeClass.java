f67ced9d651ba76bcce8550c76f656230056522d
bhawani0642 <bhawani0642@gmail.com>
2017-06-08 14:53:51 +0530

    Hascode printout
    Hascode printout

---
+++ src/hascode/HascodeClass.java
@@ -0,0 +1,14 @@
+package hascode;
+
+public class HascodeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      HascodeClass sc1= new HascodeClass();
+	  HascodeClass sc2= new HascodeClass();
+	  System.out.println(sc1.hashCode());
+	  System.out.println(sc2.hashCode());
+  
+	}
+ 
+}
